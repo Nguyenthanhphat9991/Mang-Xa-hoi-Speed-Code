e365c187f9267963dea2db0e58f7ee0af58788b9
nguyenthanhphat <nguyenthanhphat9991@gmail.com>
2021-01-12 02:58:11 +0700

    Thêm trang bài viết của người dùng
    Hiển thị các bài viết của người sử dụng

--- model/functions.php
+++ model/functions.php
@@ -70,8 +70,6 @@ function insertPost()
 				}
 			}
 		}
-
-		var_dump($user_image); die;
 	}
 }
 
@@ -377,14 +375,147 @@ function trang_post()
 								$insert = "insert into binh_luan(post_id, user_id, binh_luan,nguoi_binh_luan, thoi_gian) 
 								values('$post_id', '$user_id','$bl','$user_com_name', NOW())";
 								$run = mysqli_query($con, $insert);
-							
-								// echo "<script>alert('Your Comment added')</script>";
-	
 								echo "<script>window.open('../view/v_xem_post.php?post_id=$post_id', '_self')</script>";
 								
 							}
 						}
 				}
+		}
+}
+
+function user_posts(){
+	global $con;
+
+	if(isset($_GET['user_id'])){
+		$user_id = $_GET['user_id'];
+		}
+		$get_posts = "select * from posts where user_id='$user_id' ORDER by 1 DESC LIMIT 5";
+		$run_posts = mysqli_query($con, $get_posts);
+		while($row_posts=mysqli_fetch_array($run_posts)){
+			$url_getimageposts = '../assets/imagespost';
+			$post_id = $row_posts['post_id'];
+			$user_id = $row_posts['user_id'];
+			$post_content = $row_posts['post_content'];
+			$upload_image = $row_posts['upload_image'];
+			$post_date = $row_posts ['post_date'];
+
+			$user = "select * from user_1 where user_id='$user_id' AND posts='yes' ";
+
+			$run_user = mysqli_query($con, $user);
+			$row_user = mysqli_fetch_array($run_user);
+
+			$user_name = $row_user['user_name'];
+			$user_image = $row_user['user_image'];
+
+			if(isset($_GET['user_id'])){
+				$user_id = $_GET['user_id'];
+			}
+			$getuser = "select user_email from user_1 where user_id='$user_id'";
+
+			$run_user = mysqli_query($con, $getuser);
+			$row = mysqli_fetch_array($run_user);
+
+			$user_email = $row['user_email'];
+			$user = $_SESSION['user_email'];
+			$get_user = "select * from user_1 where user_email='$user'";
+			$run_user = mysqli_query($con, $get_user);
+			$row = mysqli_fetch_array($run_user);
+
+			$user_id = $row['user_id'];
+			$u_email = $row['user_email'];
+		
+			if($u_email != $user_email){
+				echo"<script>window.open('baivietcanhan.php?user_id=$user_id','_self')</script>";
+			}else{
+				if($post_content=="No" && strlen($upload_image) >= 1){
+					echo"
+					<div class='row'>
+						<div class='col-sm-3'>
+						</div>
+						<div id='posts' class='col-sm-6'>
+							<div class='row'>
+								<div class='col-sm-2'>
+								<p><img src='$user_image' class='img-circle' width='100px' height='100px'></p>
+								</div>
+								<div class='col-sm-6'>
+									<h3><a style='text-decoration:none; cursor:pointer;color #3897f0;' href='v_profile.php?user_id=$user_id'>$user_name</a></h3>
+									<h4><small style='color:black;'>lúc <strong>$post_date</strong></small></h4>
+								</div>
+								<div class='col-sm-4'>
+								</div>
+							</div>
+							<div class='row'>
+								<div class='col-sm-6'>
+									<img id='posts-img' src='$url_getimageposts/$upload_image' style='height:350px;'>
+								</div>
+							</div><br>
+						</div>
+						<div class='col-sm-3'>
+						</div>
+					</div><br><br>
+					";
+				}
+		
+				else if(strlen($post_content) >= 1 && strlen($upload_image) >= 1){
+					echo"
+					<div class='row'>
+						<div class='col-sm-3'>
+						</div>
+						<div id='posts' class='col-sm-6'>
+							<div class='row'>
+								<div class='col-sm-2'>
+								<p><img src='$user_image' class='img-circle' width='100px' height='100px'></p>
+								</div>
+								<div class='col-sm-6'>
+									<h3><a style='text-decoration:none; cursor:pointer;color #3897f0;' href='v_profile.php?user_id=$user_id'>$user_name</a></h3>
+									<h4><small style='color:black;'>lúc <strong>$post_date</strong></small></h4>
+								</div>
+								<div class='col-sm-4'>
+								</div>
+							</div>
+							<div class='row'>
+								<div class='col-sm-6'>
+									<p>$post_content</p>
+									<img id='posts-img' src='$url_getimageposts/$upload_image' style='height:350px;'>
+								</div>
+							</div><br>
+						</div>
+						<div class='col-sm-3'>
+						</div>
+					</div><br><br>
+					";
+				}
+		
+				else
+				{
+					echo"
+						<div class='row'>
+							<div class='col-sm-3'>
+							</div>
+							<div id='posts' class='col-sm-6'>
+								<div class='row'>
+									<div class='col-sm-2'>
+									<p><img src='$user_image' class='img-circle' width='100px' height='100px'></p>
+									</div>
+									<div class='col-sm-6'>
+										<h3><a style='text-decoration:none; cursor:pointer;color #3897f0;' href='v_profile.php?user_id=$user_id'>$user_name</a></h3>
+										<h4><small style='color:black;'>lúc <strong>$post_date</strong></small></h4>
+									</div>
+									<div class='col-sm-4'>
+									</div>
+								</div>
+								<div class='row'>
+									<div class='col-sm-6'>
+										<h3><p>$post_content</p></h3>
+									</div>
+								</div><br>
+							</div>
+							<div class='col-sm-3'>
+							</div>
+						</div><br><br>
+						";
+				}
+			}
 		}
 }
 
